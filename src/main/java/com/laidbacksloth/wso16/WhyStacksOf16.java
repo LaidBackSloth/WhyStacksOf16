6d5b6a94f1e745c2d0b646b9ff410ec4bb0d2546
LaidBackSloth42 <109034679+LaidBackSloth@users.noreply.github.com>
2022-09-27 18:55:02 +0200

    Initial commit

---
+++ src/main/java/com/laidbacksloth/wso16/WhyStacksOf16.java
@@ -0,0 +1,30 @@
+package com.laidbacksloth.wso16;
+
+import net.fabricmc.api.ModInitializer;
+import net.minecraft.item.Item;
+import net.minecraft.util.registry.Registry;
+import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.Field;
+
+public class WhyStacksOf16 implements ModInitializer {
+	@Override
+	public void onInitialize() {
+		for (Item item : Registry.ITEM) {
+			if (item.getMaxCount() == 16) {
+				try {
+					Field field;
+					try {
+						field = Item.class.getDeclaredField("maxCount");
+					} catch (NoSuchFieldException e) {
+						field = Item.class.getDeclaredField("field_8013");
+					}
+					field.setAccessible(true);
+					field.set(item, 64);
+				} catch (Exception e) {
+					LoggerFactory.getLogger("wso16").error("Could not change the max stack-size of " + Registry.ITEM.getId(item) + ", exception: " + e);
+				}
+			}
+		}
+	}
+}
